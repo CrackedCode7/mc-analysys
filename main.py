56c1acb87d093c2da6d7867e16eb7f217f379335
Evan Rinehart <ersid7@sponsellergroup.local>
2021-12-07 15:56:25 -0500

    updates. need to edit construct dict

--- main.py
+++ main.py
@@ -14,36 +14,119 @@ def get_region(chunk_x: int, chunk_z: int):
     return (math.floor(chunk_x/32), math.floor(chunk_z/32))
 
 def get_offset(chunk_x: int, chunk_z: int):
+    """Calculates the byte offset needed to find the chunk at the given coordinates in the header"""
     return 4*((chunk_x%32)+(chunk_z%32)*32)
 
-print(get_region(-22, 79))
-print(get_offset(-22, 79))
+def read_chunk_data(chunk_x: int, chunk_z: int, data: bytes):
+    offset = get_offset(chunk_x, chunk_z)
+    chunk_offset = int.from_bytes(data[offset:offset+3], 'big')*4096 # offset for chunk data in bytes
+    chunk_length = int.from_bytes(data[offset+3:offset+4], 'big')*4096 # chunk length in bytes
+    timestamp = int.from_bytes(data[offset+4100:offset+4104], 'big') # time since epoch of last update
+    
+    remaining_length = struct.unpack('>i', data[chunk_offset:chunk_offset+4])[0] # exact length in bytes of the remaining chunk data
+    
+    byte_index = chunk_offset+4
+    compression_scheme = int.from_bytes(data[byte_index:byte_index+1], 'big')
+    byte_index += 1
+    
+    last_byte = byte_index+remaining_length-1
+    
+    if compression_scheme == 2:
+        return zlib.decompress(data[byte_index:last_byte])
+    else:
+        return
+
+
+class TAG_End:
+    pass
+    
+
+class TAG_Byte:
+    pass
+
+
+class TAG_Short:
+    pass
+
+
+class TAG_Int:
+    pass
+
+
+class TAG_Long:
+    pass
+
+
+class TAG_Float:
+    pass
+
+
+class TAG_Double:
+    pass
+
+
+class TAG_Byte_Array:
+    pass
+
+
+class TAG_String:
+    pass
+
+
+class TAG_List:
+    pass
+
+
+class TAG_Compound:
+    
+    def __init__(self, chunk_data):
+    
+        self.dict = {}
+        self.buffer_index = 0
+        
+        self.type_id = int.from_bytes(chunk_data[0:1], 'big')
+
+        string_length = int.from_bytes(chunk_data[1:3], 'big')
+        if string_length != 0:
+            self.name = chunk_data[3:3+string_length].decode('utf-8')
+        else:
+            self.name = ''
+        
+        self.buffer = chunk_data[3+string_length:]
+        
+        self.construct_dict()
+    
+    def construct_dict(self):
+        
+        while self.buffer_index < len(self.buffer):
+            
+            # Handle when a tag is encountered
+            type_id = int.from_bytes(self.buffer[self.buffer_index:self.buffer_index+1], 'big')
+            self.buffer_index += 1
+            string_length = int.from_bytes(self.buffer[self.buffer_index:self.buffer_index+2], 'big')
+            self.buffer_index += 2
+            if string_length != 0:
+                name = self.buffer[self.buffer_index:self.buffer_index+string_length].decode('utf-8')
+                self.buffer_index += string_length
+            else:
+                name = ''
+                break
+            
+            self.dict[name] = None
+
+
+class TAG_Int_Array:
+    pass
+
+
+class TAG_Long_Array:
+    pass
+
 
 with open('r.-1.2.mca', 'rb') as f:
-    """
-    f.read(1960)
 
-    chunk_offset = f.read(3)
-    print(int.from_bytes(chunk_offset, 'big'), 'is the beginning of the chunk (in 4kB sectors)')
-
-    length = f.read(1)
-    print(int.from_bytes(length, 'big'), 'is the number of 4kB sectors in the chunk')
-
-    f.read(4096)
-
-    timestamp = f.read(4)
-    print(time.time()-struct.unpack('>i', timestamp)[0], 'seconds since chunk was last updated.')"""
-
-    f.read(4096*1779)
-
-    rem_length = struct.unpack('>i', f.read(4))
-    print(rem_length, 'is the remaining number of bytes in the chunk')
-
-    compression_scheme = int.from_bytes(f.read(1), 'big')
-    print(compression_scheme, 'is the compression scheme')
-
-    compressed_data = f.read(rem_length[0]-1)
-    decompressed_data = zlib.decompress(compressed_data)
-
-    type_id = decompressed_data[0]
-    print(type_id, 'is the type id of the first tag (TAG_Compound)')
+    data = f.read()
+    chunk_data = read_chunk_data(-22, 79, data)
+    
+    cmpd = TAG_Compound(chunk_data)
+    print(len(cmpd.buffer))
